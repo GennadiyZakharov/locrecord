5c66bf339642b95ffd3f40fbf06b4413ec23f291
Gena <Gena-Za@yandex.ru>
2021-10-31 22:33:49 +0300

    Fixed a bug with set_time_stamp() Applied PEP style guidelines

--- locrecord.py
+++ locrecord.py
@@ -10,10 +10,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 rtspStr = 'rtsp://'
 vlcStr = '''vlc'''
 
-runTimeStr = '''--run-time {}'''
-sout1Str = '''--sout="#duplicate{dst=std{access=file,mux=avi,'''
+run_time_str = '''--run-time {}'''
+sout_1_str = '''--sout="#duplicate{dst=std{access=file,mux=avi,'''
 dstStr = '''dst="{}"'''
-sout2Str = '''}, dst=display}" vlc://quit'''
+sout_2_str = '''}, dst=display}" vlc://quit'''
 
 title = "VLC video recorder"
 
@@ -21,14 +21,16 @@ title = "VLC video recorder"
 class MainWindow(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        self.is_vlc_running = False
         self.setWindowTitle(title)
         self.setObjectName("MainWindow")
         self.fileName = ""
         self.cameras = []
         self.cameraNumber = 0
-        self.loadCameras()
+        self.load_cameras()
+        # noinspection PySimplifyBooleanCheck
         if self.cameras == []:
-            print('No cameras avaliable, exiting')
+            print('No cameras available, exiting')
             exit(-1)
 
         layout = QtWidgets.QGridLayout()
@@ -37,28 +39,29 @@ class MainWindow(QtWidgets.QDialog):
         layout.addWidget(camera_label, 0, 0)
         camera_selector = QtWidgets.QComboBox()
         camera_selector.addItems([camera['name'] for camera in self.cameras])
-        camera_selector.currentIndexChanged.connect(self.selectCamera)
+        # noinspection PyUnresolvedReferences
+        camera_selector.currentIndexChanged.connect(self.select_camera)
         layout.addWidget(camera_selector, 0, 2)
         camera_selector.setCurrentIndex(0)
 
         file_name_label = QtWidgets.QLabel("Output file Name: ")
         layout.addWidget(file_name_label, 1, 0)
-        fileNameBrowse = QtWidgets.QPushButton("Browse...")
-        fileNameBrowse.clicked.connect(self.browse)
-        layout.addWidget(fileNameBrowse, 1, 2)
+        file_name_browse = QtWidgets.QPushButton("Browse...")
+        file_name_browse.clicked.connect(self.browse)
+        layout.addWidget(file_name_browse, 1, 2)
         self.fileNameEdit = QtWidgets.QLineEdit()
-        self.fileNameEdit.textChanged.connect(self.checkFileName)
+        self.fileNameEdit.textChanged.connect(self.set_output_file_name)
         layout.addWidget(self.fileNameEdit, 2, 0, 1, 3)
         #
-        durationLabel = QtWidgets.QLabel('Duration, min:')
-        layout.addWidget(durationLabel, 3, 0)
+        duration_label = QtWidgets.QLabel('Duration, min:')
+        layout.addWidget(duration_label, 3, 0)
         self.durationSpinBox = QtWidgets.QSpinBox()
         self.durationSpinBox.setMinimum(5)
         self.durationSpinBox.setMaximum(120)
         layout.addWidget(self.durationSpinBox, 3, 1)
-        setTimeStampButton = QtWidgets.QPushButton("Set time stamp")
-        setTimeStampButton.clicked.connect(self.setTimeStamp)
-        layout.addWidget(setTimeStampButton, 3, 2)
+        set_time_stamp_button = QtWidgets.QPushButton("Set time stamp")
+        set_time_stamp_button.clicked.connect(self.set_time_stamp)
+        layout.addWidget(set_time_stamp_button, 3, 2)
 
         self.previewButton = QtWidgets.QPushButton("Preview")
         layout.addWidget(self.previewButton, 4, 0)
@@ -70,7 +73,7 @@ class MainWindow(QtWidgets.QDialog):
 
         self.setLayout(layout)
         #
-        self.controls = [fileNameBrowse, self.fileNameEdit, self.durationSpinBox, setTimeStampButton,
+        self.controls = [file_name_browse, self.fileNameEdit, self.durationSpinBox, set_time_stamp_button,
                          self.previewButton, self.processButton]
         # Restoring settings
         settings = QtCore.QSettings()
@@ -83,81 +86,80 @@ class MainWindow(QtWidgets.QDialog):
         if settings.value("Geometry") is not None:
             self.restoreGeometry(settings.value("Geometry"))
 
-    def loadCameras(self):
+    def load_cameras(self):
         print('loading cameras')
         config = configparser.ConfigParser()
-        filelist = config.read("./cameras.ini")
+        config.read("./cameras.ini")
         for camera in config.sections():
             print(camera)
-            cameraDict = {'name': camera}
+            camera_dict = {'name': camera}
             options = config.options(camera)
             for option in options:
                 try:
-                    cameraDict[option] = config.get(camera, option)
-                    if cameraDict[option] == -1:
+                    camera_dict[option] = config.get(camera, option)
+                    if camera_dict[option] == -1:
                         print("skip: %s" % option)
                 except:
                     print("exception on %s!" % option)
-                    del cameraDict[option]
-            print(cameraDict)
-            self.cameras.append(cameraDict)
+                    del camera_dict[option]
+            print(camera_dict)
+            self.cameras.append(camera_dict)
 
-    def selectCamera(self, cameraNumber):
-        self.cameraNumber = cameraNumber
+    def select_camera(self, camera_number):
+        self.cameraNumber = camera_number
 
-    def enableControls(self, enabled):
+    def enable_controls(self, enabled):
         for item in self.controls:
             item.setEnabled(enabled)
 
     def browse(self):
 
-        currentDir = QtCore.QFileInfo(self.fileName).absolutePath() \
+        current_dir: str = QtCore.QFileInfo(self.fileName).absolutePath() \
             if self.fileName != "" else "."
         # Executing standard open dialog
         fname = QtWidgets.QFileDialog.getSaveFileName(self,
                                                       QtWidgets.QApplication.applicationName() + " - Choose file",
-                                                      currentDir, "Video files (*.avi)")
+                                                      current_dir, "Video files (*.avi)")
         print(fname)
         if fname != "":
             self.fileNameEdit.setText(fname[0])
 
-    def checkFileName(self, name):
+    def set_output_file_name(self, name):
         self.fileName = name
         self.processButton.setEnabled(name != '')
 
-    def setTimeStamp(self):
-        fileInfo = QtCore.QFileInfo(self.fileName)
-        baseName = fileInfo.fileName()
-        re.sub(r'^20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]_', '', baseName)
-        #baseName = baseName.remove(QtCore.QRegExp('^20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]_'))
-        currentDate = QtCore.QDate.currentDate().toString('yyyy-MM-dd') + '_'
-        fullName = os.path.join(fileInfo.absolutePath(), currentDate + baseName)
-        self.fileNameEdit.setText(fullName)
+    def set_time_stamp(self):
+        file_info = QtCore.QFileInfo(self.fileName)
+        base_name = file_info.fileName()
+        base_name = re.sub(r'^20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]_', '', base_name)
+        current_date = QtCore.QDate.currentDate().toString('yyyy-MM-dd') + '_'
+        full_name = os.path.join(file_info.absolutePath(), current_date + base_name)
+        self.fileNameEdit.setText(full_name)
 
-    def urlStr(self):
+    def make_camera_url(self):
         camera = self.cameras[self.cameraNumber]
-        creditals = camera['creditals'] + '@' if camera['creditals'] != '' else ''
-        cameraUrl = rtspStr + creditals + camera['ip'] + ':' + str(camera['port']) + '/' + camera['address']
-        print('Camera:' + camera['name'] + ' ' + cameraUrl)
-        return cameraUrl
+        credentials = camera['credentials'] + '@' if camera['credentials'] != '' else ''
+        camera_url = rtspStr + credentials + camera['ip'] + ':' + str(camera['port']) + '/' + camera['address']
+        print('Camera:' + camera['name'] + ' ' + camera_url)
+        return camera_url
 
     def preview(self):
-        self.startVlc(vlcStr + ' ' + self.urlStr())
+        self.start_vlc(vlcStr + ' ' + self.make_camera_url())
 
-    def startVlc(self, runString):
+    def start_vlc(self, vlc_run_string):
         print('Starting VLC')
-        print(runString)
+        print(vlc_run_string)
         vlc = QtCore.QProcess(self)
-        vlc.finished.connect(self.vlcFinished)
-        self.runflag = True
-        self.enableControls(False)
-        vlc.start(runString)
-        while self.runflag:
+        vlc.finished.connect(self.vlc_finished)
+        self.is_vlc_running = True
+        self.enable_controls(False)
+        vlc.start(vlc_run_string)
+        while self.is_vlc_running:
             QtWidgets.QApplication.processEvents()
-        self.enableControls(True)
+        self.enable_controls(True)
 
-    def vlcFinished(self):
-        self.runflag = False
+    def vlc_finished(self):
+        self.is_vlc_running = False
 
     def process(self):
         if QtCore.QFile(self.fileName).exists():
@@ -165,9 +167,9 @@ class MainWindow(QtWidgets.QDialog):
                                     'File already exsts!<p><b>' + self.fileName + '</b><p>Please select another one')
             return
         time = self.durationSpinBox.value() * 60
-        runString = vlcStr + ' ' + self.urlStr() + ' ' + runTimeStr.format(time) + ' ' + \
-                    sout1Str + dstStr.format(self.fileName.strip()) + sout2Str
-        self.startVlc(runString)
+        vlc_run_string = vlcStr + ' ' + self.make_camera_url() + ' ' + run_time_str.format(time) + ' ' + \
+                         sout_1_str + dstStr.format(self.fileName.strip()) + sout_2_str
+        self.start_vlc(vlc_run_string)
 
     def closeEvent(self, event):
         # Save settings and exit
@@ -183,8 +185,8 @@ def main():
     app.setOrganizationDomain("infran.ru")
     app.setApplicationName(title)
     # app.setWindowIcon(QIcon(":/icon.png"))
-    mainWindow = MainWindow()
-    mainWindow.show()
+    main_window = MainWindow()
+    main_window.show()
     return app.exec_()
 
 
