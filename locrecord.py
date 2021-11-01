d419e65cd141e45862412622892e77748b5ebb16
Gena <Gena-Za@yandex.ru>
2021-11-01 11:39:13 +0300

    Moved to UI creation with QtDesigner

--- locrecord.py
+++ locrecord.py
@@ -6,6 +6,8 @@ import configparser
 import regex as re
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+from locrecord_pyuic import Ui_MainWindow
+
 # === Cameras ===
 rtspStr = 'rtsp://'
 vlcStr = '''vlc'''
@@ -33,56 +35,32 @@ class MainWindow(QtWidgets.QDialog):
             print('No cameras available, exiting')
             exit(-1)
 
-        layout = QtWidgets.QGridLayout()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        camera_label = QtWidgets.QLabel("Camera: ")
-        layout.addWidget(camera_label, 0, 0)
-        camera_selector = QtWidgets.QComboBox()
-        camera_selector.addItems([camera['name'] for camera in self.cameras])
+        self.ui.camera_selector.addItems([camera['name'] for camera in self.cameras])
         # noinspection PyUnresolvedReferences
-        camera_selector.currentIndexChanged.connect(self.select_camera)
-        layout.addWidget(camera_selector, 0, 2)
-        camera_selector.setCurrentIndex(0)
+        self.ui.camera_selector.currentIndexChanged.connect(self.select_camera)
+        self.ui.camera_selector.setCurrentIndex(0)
 
-        file_name_label = QtWidgets.QLabel("Output file Name: ")
-        layout.addWidget(file_name_label, 1, 0)
-        file_name_browse = QtWidgets.QPushButton("Browse...")
-        file_name_browse.clicked.connect(self.browse)
-        layout.addWidget(file_name_browse, 1, 2)
-        self.fileNameEdit = QtWidgets.QLineEdit()
-        self.fileNameEdit.textChanged.connect(self.set_output_file_name)
-        layout.addWidget(self.fileNameEdit, 2, 0, 1, 3)
+        self.ui.file_name_browse.clicked.connect(self.browse)
+        self.ui.fileNameEdit.textChanged.connect(self.set_output_file_name)
         #
-        duration_label = QtWidgets.QLabel('Duration, min:')
-        layout.addWidget(duration_label, 3, 0)
-        self.durationSpinBox = QtWidgets.QSpinBox()
-        self.durationSpinBox.setMinimum(5)
-        self.durationSpinBox.setMaximum(120)
-        layout.addWidget(self.durationSpinBox, 3, 1)
-        set_time_stamp_button = QtWidgets.QPushButton("Set time stamp")
-        set_time_stamp_button.clicked.connect(self.set_time_stamp)
-        layout.addWidget(set_time_stamp_button, 3, 2)
+        self.ui.set_time_stamp_button.clicked.connect(self.set_time_stamp)
 
-        self.previewButton = QtWidgets.QPushButton("Preview")
-        layout.addWidget(self.previewButton, 4, 0)
-        self.previewButton.clicked.connect(self.preview)
+        self.ui.previewButton.clicked.connect(self.preview)
         #
-        self.processButton = QtWidgets.QPushButton("<< Start >>")
-        layout.addWidget(self.processButton, 4, 1, 1, 2)
-        self.processButton.clicked.connect(self.process)
+        self.ui.processButton.clicked.connect(self.process)
 
-        self.setLayout(layout)
         #
-        self.controls = [file_name_browse, self.fileNameEdit, self.durationSpinBox, set_time_stamp_button,
-                         self.previewButton, self.processButton]
+        self.controls = [self.ui.file_name_browse, self.ui.fileNameEdit, self.ui.durationSpinBox, self.ui.set_time_stamp_button,
+                         self.ui.previewButton, self.ui.processButton]
         # Restoring settings
         settings = QtCore.QSettings()
-        self.fileNameEdit.setText(settings.value("LastFile") if settings.value("LastFile") is not None else "")
+        self.ui.fileNameEdit.setText(settings.value("LastFile") if settings.value("LastFile") is not None else "")
         duration = settings.value("Duration")
         if duration is not None:
-            self.durationSpinBox.setValue(int(duration))
-        else:
-            self.durationSpinBox.setValue(60)
+            self.ui.durationSpinBox.setValue(int(duration))
         if settings.value("Geometry") is not None:
             self.restoreGeometry(settings.value("Geometry"))
 
@@ -122,11 +100,11 @@ class MainWindow(QtWidgets.QDialog):
                                                       current_dir, "Video files (*.avi)")
         print(fname)
         if fname != "":
-            self.fileNameEdit.setText(fname[0])
+            self.ui.fileNameEdit.setText(fname[0])
 
     def set_output_file_name(self, name):
         self.fileName = name
-        self.processButton.setEnabled(name != '')
+        self.ui.processButton.setEnabled(name != '')
 
     def set_time_stamp(self):
         file_info = QtCore.QFileInfo(self.fileName)
@@ -134,13 +112,12 @@ class MainWindow(QtWidgets.QDialog):
         base_name = re.sub(r'^20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]_', '', base_name)
         current_date = QtCore.QDate.currentDate().toString('yyyy-MM-dd') + '_'
         full_name = os.path.join(file_info.absolutePath(), current_date + base_name)
-        self.fileNameEdit.setText(full_name)
+        self.ui.fileNameEdit.setText(full_name)
 
     def make_camera_url(self):
         camera = self.cameras[self.cameraNumber]
         credentials = camera['credentials'] + '@' if camera['credentials'] != '' else ''
         camera_url = rtspStr + credentials + camera['ip'] + ':' + str(camera['port']) + '/' + camera['address']
-        print('Camera:' + camera['name'] + ' ' + camera_url)
         return camera_url
 
     def preview(self):
@@ -166,7 +143,7 @@ class MainWindow(QtWidgets.QDialog):
             QtGui.QMessageBox.about(self, "VLC-Based video recorder",
                                     'File already exsts!<p><b>' + self.fileName + '</b><p>Please select another one')
             return
-        time = self.durationSpinBox.value() * 60
+        time = self.ui.durationSpinBox.value() * 60
         vlc_run_string = vlcStr + ' ' + self.make_camera_url() + ' ' + run_time_str.format(time) + ' ' + \
                          sout_1_str + dstStr.format(self.fileName.strip()) + sout_2_str
         self.start_vlc(vlc_run_string)
@@ -175,7 +152,7 @@ class MainWindow(QtWidgets.QDialog):
         # Save settings and exit
         settings = QtCore.QSettings()
         settings.setValue("LastFile", self.fileName)
-        settings.setValue("Duration", self.durationSpinBox.value())
+        settings.setValue("Duration", self.ui.durationSpinBox.value())
         settings.setValue("Geometry", QtCore.QVariant(self.saveGeometry()))
 
 
